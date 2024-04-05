c29436abb45a88931f0dbfb57d32d229a4abbd03
Yogesh Raut <yogesh@Yogeshs-MacBook-Air.local>
2024-04-05 15:00:51 +0530

    41 final and const special kinds of Variable

--- lib/grandient_container.dart
+++ lib/grandient_container.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/styled_text.dart';
 
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         gradient: LinearGradient(
-          colors: const [
+          colors: [
             Color.fromARGB(255, 26, 2, 80),
             Color.fromARGB(255, 45, 7, 98)
           ],
